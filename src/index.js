ce70a104b4638c1e6315372f773ad22d5b9425a3
surajpagad <suraj.pagad61@gmail.com>
2018-01-06 19:04:25 +0530

    fifth commit

--- src/index.js
+++ src/index.js
@@ -29,7 +29,6 @@ class App extends React.Component {
   fetchWeather() {
     var tempC = 0;
     var tempF = 0;
-
     var weatherAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${this
       .lat}&lon=${this
       .long}&units=metric&appid=924d4013269e4852627c3106302c806b`;
@@ -48,16 +47,14 @@ class App extends React.Component {
           tempC,
           tempF,
           description: data.weather[0].description,
-          iconCode: data.weather[0].icon,
           weatherMain: data.weather[0].id
         });
-        this.setState({ status: "loaded" });
+        this.setState({ status: "loaded"});
       });
   }
 
   getPlace() {
     this.setState({ status: "loading...", searchText: this.input.value });
-
     var place = this.autocomplete.getPlace();
     if(place.geometry){
       this.setState({
@@ -73,6 +70,7 @@ class App extends React.Component {
   }
 
   handleScriptLoad() {
+    /*Hit enter without selecting dropdown, select first option*/
     window.google.maps.event.addDomListener(this.input, "keydown", function(e) {
       if (
         e.keyCode === 13 &&
@@ -80,8 +78,7 @@ class App extends React.Component {
         !e.triggered
       ) {
         window.google.maps.event.trigger(this, "keydown", {
-          keyCode: 40,
-          automatedDown: true
+          keyCode: 40
         });
         window.google.maps.event.trigger(this, "keydown", {
           keyCode: 13,
@@ -89,14 +86,15 @@ class App extends React.Component {
         });
       }
     });
+    /*call autocomplete on input element*/
     this.autocomplete = new window.google.maps.places
       .Autocomplete(/** @type {!HTMLInputElement} */ (this.input), {
       types: ["geocode"]
     });
     this.autocomplete.addListener("place_changed", this.getPlace);
-    this.setState({ status: "loading..." });
     if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(position => {
+      this.setState({ status: "loading..." });
+      navigator.geolocation.getCurrentPosition(/* success*/position => {
         this.lat = position.coords.latitude;
         this.long = position.coords.longitude;
         this.setState({ lat: this.lat, long: this.long });
@@ -111,14 +109,21 @@ class App extends React.Component {
             this.setState({ address: json.results[0].formatted_address });
           });
         this.fetchWeather();
+      },/*error*/()=>{
+        this.setState({ status: "Enter an address" });
       });
     } else {
-      this.setState({ status: "Enter address" });
+      /*navigator not found*/
+      this.setState({ status: "Enter an address" });
     }
   }
 
   handleChange(e) {
-    this.setState({ searchText: e.target.value });
+    this.setState({ searchText: e.target.value,status: e.target.value?this.state.status:''});
+  }
+
+  handleOnClear() {
+    this.setState({searchText:'',status:''})
   }
 
   render() {
@@ -135,15 +140,17 @@ class App extends React.Component {
             ref={input => {
               this.input = input;
             }}
+            value={this.state.searchText}
             placeholder="Enter your address"
             type="text"
           />
+          {(this.state.searchText.length)?<span className='input-clear' onClick={this.handleOnClear.bind(this)}>&times;</span>:null}
           <span className="input-highlight">
             {this.state.searchText.replace(/ /g, "\u00a0")}
           </span>
         </div>
 
-        {this.state.status === "loaded" ? (
+        {(this.state.status === "loaded") ? (
           <div>
             <p>{this.state.description}</p>
             {this.state.on ? (
